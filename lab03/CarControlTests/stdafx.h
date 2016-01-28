fe400c49c94c1574853b510cc5cd053f34eebadc
Und3rMySk1n <undermyskin@inbox.ru>
2016-01-28 12:43:05 +0300

    Настроил логгирование тестов.

--- lab03/CarControlTests/stdafx.h
+++ lab03/CarControlTests/stdafx.h
@@ -9,12 +9,12 @@
 
 #include <stdio.h>
 #include <tchar.h>
-
-///************************************************
-#define BOOST_TEST_MODULE CarControlTestsModule
-#define BOOST_TEST_INCLUDED
-#pragma warning (disable: 4702)
-#include <boost\test\unit_test.hpp>
-///************************************************
+#include <boost/algorithm/string/replace.hpp>
 
 // TODO: reference additional headers your program requires here
+
+///************************************************
+#define BOOST_TEST_INCLUDED
+#pragma warning (disable: 4702)
+#include <boost/test/unit_test.hpp>
+///************************************************
