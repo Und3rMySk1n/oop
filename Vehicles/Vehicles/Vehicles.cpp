62ccfbf3675f571bd3ff58d18673f35df24a3ab0
Und3rMySk1n <undermyskin@inbox.ru>
2016-02-18 21:34:25 +0300

    Добавил работу с примером использования виртуальных методов класса.

---
+++ Vehicles/Vehicles/Vehicles.cpp
@@ -0,0 +1,92 @@
+// Vehicles.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+class CVehicle
+{
+public:
+	virtual void Move(unsigned int distance) const
+	{
+		cout << "Moving " << distance << " km" << endl;
+	}
+};
+
+class CFlyingVehicle 
+	: public CVehicle
+{
+public:
+	virtual void Move(unsigned int distance) const
+	{
+		cout << "Flying " << distance << " km" << endl;
+	}
+};
+
+class CSailingVehicle 
+	: public CVehicle
+{
+public:
+	virtual void Move(unsigned int distance) const
+	{
+		cout << "Sailing " << distance << " km" << endl;
+	}
+};
+
+class CRidingVehicle 
+	: public CVehicle
+{
+public:
+	virtual void Move(unsigned int distance) const
+	{
+		cout << "Riding " << distance << " km" << endl;
+	}
+};
+
+class CHelicopter 
+	: public CFlyingVehicle
+{};
+
+class CAirplane 
+	: public CFlyingVehicle
+{};
+
+class CBus 
+	: public CRidingVehicle
+{};
+
+class CCar 
+	: public CRidingVehicle
+{};
+
+class CBoat 
+	: public CSailingVehicle
+{};
+
+int main(int argc, char* argv[])
+{
+	CHelicopter helicopter;
+	cout << "Helicopter: ";
+	helicopter.Move(30);
+
+	CAirplane airplane;
+	cout << "Airplane: ";
+	airplane.Move(800);
+
+	CCar car;
+	cout << "Car: ";
+	car.Move(100);	
+
+	CBus bus;
+	cout << "Bus: ";
+	bus.Move(500);
+
+	CBoat boat;
+	cout << "Boat: ";
+	boat.Move(25);
+
+	return 0;
+}
+
