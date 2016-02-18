55e1bc89cee9e8c898b55dc05d890011bb5abe9a
Und3rMySk1n <undermyskin@inbox.ru>
2016-02-18 19:32:44 +0300

    Добавил текст в main.

--- lab06/Solve3/Solve3.cpp
+++ lab06/Solve3/Solve3.cpp
@@ -11,6 +11,11 @@ int main(int argc, char* argv[])
 	try
 	{
 		EquationRoots3 result = Solve3(4.0, 5.0, 7.0, 2.0);
+		cout << result.numRoots << endl;
+		for (int i = 0; i < result.numRoots; i++)
+		{
+			cout << "root[" << i << "] = " << result.roots[i] << endl;
+		}
 	}
 	catch (const std::invalid_argument &e)
 	{
